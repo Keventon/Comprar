f6305f62a93914a27436e48ee878723fd3c78c1d
Keventon <kevertonguimaraes19@gmail.com>
2025-06-02 18:41:08 -0300

    criando tela principal

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#D0D2D8",
+    paddingTop: 62,
+    alignItems: "center",
+  },
+  logo: {
+    height: 34,
+    width: 134,
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    paddingTop: 32,
+    marginTop: 24,
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E4E6EC",
+    paddingBottom: 12,
+  },
+  clearButton: {
+    marginLeft: "auto",
+  },
+  clearText: {
+    color: "#828282",
+    fontSize: 12,
+    fontWeight: "600",
+  },
+});
